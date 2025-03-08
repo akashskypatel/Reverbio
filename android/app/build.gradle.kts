013ca15096e6df407fb122c2b916930725c353f2
akashskypatel <akashskypatel@gmail.com>
2025-03-08 12:37:24 -0800

    fix android build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.gokadzev.musify"
+    namespace = "com.ruchan.musify"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.gokadzev.musify"
+        applicationId = "com.ruchan.musify"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
---
+++ android/app/src/main/kotlin/com/ruchan/musify/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.ruchan.musify
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
