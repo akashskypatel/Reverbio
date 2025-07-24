dd186aa4d8a38e212fd2c43319bebb2754e07c4b
akashskypatel <akashskypatel@gmail.com>
2025-07-24 15:22:55 -0700

    fixed my name 😅

--- lib/widgets/song_bar.dart
+++ lib/widgets/song_bar.dart
@@ -1,5 +1,5 @@
 /*
- *     Copyright (C) 2025 Akashy Patel
+ *     Copyright (C) 2025 Akash Patel
  *
  *     Reverbio is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
