414d72fbc385e54eafafc159ac3ff46b0a35f14a
akashskypatel <akashskypatel@gmail.com>
2025-09-25 16:47:40 -0700

    add clear all notification button

--- lib/widgets/notification_log.dart
+++ lib/widgets/notification_log.dart
@@ -1,10 +1,12 @@
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:reverbio/extensions/l10n.dart';
 import 'package:reverbio/utilities/common_variables.dart';
 import 'package:reverbio/utilities/flutter_bottom_sheet.dart';
 import 'package:reverbio/utilities/flutter_toast.dart';
 import 'package:reverbio/utilities/utils.dart';
 import 'package:reverbio/widgets/bottom_sheet_bar.dart';
+import 'package:reverbio/widgets/section_header.dart';
 import 'package:reverbio/widgets/spinner.dart';
 
 Future<void> showNotificationLog(BuildContext context) async {
@@ -20,30 +22,70 @@ Future<void> showNotificationLog(BuildContext context) async {
               }).toList()
               ..sort((a, b) => a['index'].compareTo(b['index']));
         final _logKeys = _logList.map((e) => e['id']).toList();
-        return ListView.builder(
-          shrinkWrap: true,
-          physics: const BouncingScrollPhysics(),
-          padding: commonListViewBottomPadding,
-          itemCount: _logKeys.length,
-          itemBuilder: (context, index) {
-            final notification = notificationLog[_logKeys[index]];
-            final borderRadius = getItemBorderRadius(index, _logKeys.length);
-            final progress = notificationLog[notification['id']]?['data'];
-            final message = notification['message'];
-            final dateTime = notification['dateTime'];
-            if (progress is ValueNotifier<int>) {
-              return ValueListenableBuilder(
-                valueListenable: progress,
-                builder: (context, value, child) {
-                  final action = Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      SizedBox.square(
-                        dimension: 35,
-                        child: Spinner(value: value / 100),
-                      ),
-                      Text('$value%'),
-                    ],
+        return Column(
+          children: [
+            SectionHeader(
+              title: context.l10n!.notifications,
+              expandedActions: [
+                IconButton(
+                  onPressed: () async {
+                    notificationLog.removeWhere(
+                      (key, value) => !(value['data'] is ValueNotifier<int>),
+                    );
+                    if (context.mounted) setState(() {});
+                  },
+                  icon: const Icon(Icons.clear_all),
+                  iconSize: listHeaderIconSize,
+                  color: _theme.colorScheme.primary,
+                ),
+              ],
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              physics: const BouncingScrollPhysics(),
+              padding: commonListViewBottomPadding,
+              itemCount: _logKeys.length,
+              itemBuilder: (context, index) {
+                final notification = notificationLog[_logKeys[index]];
+                final borderRadius = getItemBorderRadius(
+                  index,
+                  _logKeys.length,
+                );
+                final progress = notificationLog[notification['id']]?['data'];
+                final message = notification['message'];
+                final dateTime = notification['dateTime'];
+                if (progress is ValueNotifier<int>) {
+                  return ValueListenableBuilder(
+                    valueListenable: progress,
+                    builder: (context, value, child) {
+                      final action = Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          SizedBox.square(
+                            dimension: 35,
+                            child: Spinner(value: value / 100),
+                          ),
+                          Text('$value%'),
+                        ],
+                      );
+                      return BottomSheetBar(
+                        '$message · ${dateTime != null ? formatRelativeTime(dateTime) : ''}',
+                        inactivatedColor,
+                        borderRadius: borderRadius,
+                        actions: [action],
+                      );
+                    },
+                  );
+                } else {
+                  final action = IconButton(
+                    icon: const Icon(FluentIcons.dismiss_24_filled),
+                    onPressed: () {
+                      notificationLog.remove(_logKeys[index]);
+                      if (context.mounted)
+                        setState(() {
+                          notificationLogLength.value = notificationLog.length;
+                        });
+                    },
                   );
                   return BottomSheetBar(
                     '$message · ${dateTime != null ? formatRelativeTime(dateTime) : ''}',
@@ -51,27 +93,10 @@ Future<void> showNotificationLog(BuildContext context) async {
                     borderRadius: borderRadius,
                     actions: [action],
                   );
-                },
-              );
-            } else {
-              final action = IconButton(
-                icon: const Icon(FluentIcons.dismiss_24_filled),
-                onPressed: () {
-                  notificationLog.remove(_logKeys[index]);
-                  if (context.mounted)
-                    setState(() {
-                      notificationLogLength.value = notificationLog.length;
-                    });
-                },
-              );
-              return BottomSheetBar(
-                '$message · ${dateTime != null ? formatRelativeTime(dateTime) : ''}',
-                inactivatedColor,
-                borderRadius: borderRadius,
-                actions: [action],
-              );
-            }
-          },
+                }
+              },
+            ),
+          ],
         );
       },
     ),
